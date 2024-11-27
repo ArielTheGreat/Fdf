7a0385c832dcf364d271051bd3a15e30d9c0fdfb
frocha <frocha@student.42heilbronn.de>
2024-11-27 13:32:49 +0100

    remove not used functions, since they were commented and not used

--- memory_allocation.c
+++ memory_allocation.c
@@ -50,59 +50,9 @@ void allocate_memory(int width, int length, int ***matrix_numbers)
     }
 }
 
-// void set_numbers_for_matrix(int length, int ***matrix_numbers, int fd)
-// {
-//     // int i;
-//     int j;
-//     char *line;
-//     char **words;
-
-//     // i = 0;
-//     // while(i < length)
-//     // {
-//         j = 0;
-//         length = 2;
-//         line = get_next_line(fd);
-//         printf("%s\n",line);
-//         // if (!line) {
-//         //     perror("Error reading line from file");
-//         //     break;
-//         // }
-//         words = ft_split(line, ' ');
-//         free(line);
-//         while(words[j])
-//         {
-//             (*matrix_numbers)[0][j] = ft_atoi(words[j]);
-//             j++;
-//         }
-//         free_split_arrays(words);
-//     //     i++;
-//     // }
-// }
-
 void    allocate_memory_and_set_memory(t_matrix_info **matrix, int ***matrix_numbers, int fd)
 {
     set_width_matrix(matrix, fd);
     set_length_matrix(matrix, fd);
     allocate_memory((*matrix)->width, (*matrix)->length + 1, matrix_numbers);
-    // set_numbers_for_matrix((*matrix)->length, matrix_numbers, fd);
-    // // int i;
-    // int j;
-    // j = 0;
-    // while(j < (*matrix)->width)
-    // {
-    //     printf("%i ",(*matrix_numbers)[0][j]);
-    //     j++;
-    // }
-    // while(i < (*matrix)->length - 1)
-    // {
-    //     j = 0;
-    //     while(j < (*matrix)->width)
-    //     {
-    //         printf("%i ",(*matrix_numbers)[i][j]);
-    //         j++;
-    //     }
-    //     printf("\n");
-    //     i++;
-    // }
 }
