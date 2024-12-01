27371ea119e979c46600c5b9471fb68b5b532fee
frocha <frocha@student.42heilbronn.de>
2024-12-01 14:10:12 +0100

    corrected value for applying conversion from hex to int

--- fdf.c
+++ fdf.c
@@ -12,7 +12,22 @@
 
 #include "fdf.h"
 
-int get_index_hex(char character)
+int get_base_multiplication(int size_str)
+{
+    int result;
+    int i;
+
+    i = 0;
+    result = 1;
+    while(i < size_str)
+    {
+        result = result * 16;
+        i++;
+    }
+    return (result);
+}
+
+int get_index_hex(char character, int size_str)
 {
     char *hex_values;
     int i;
@@ -21,20 +36,23 @@ int get_index_hex(char character)
     i = 0;
     while(hex_values[i] != character)
         i++;
-    return (i);
+    return (i * get_base_multiplication(size_str));
 }
 
 int convert_hex_to_int(char *color_element)
 {
     int i;
     int result;
+    int size_str;
 
     i = 2;
-    result = 1;
+    result = 0;
+    size_str = ft_strlen(color_element+i) - 1;
     while(color_element[i] != '\0')
     {
-        result = result * get_index_hex(color_element[i]);
+        result = result + get_index_hex(color_element[i], size_str);
         i++;
+        size_str--;
     }
     return (result);
 }
