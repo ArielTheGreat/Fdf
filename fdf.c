ef4e05c8480dbb26957d5392e2ed6643ea4f7926
frocha <frocha@student.42heilbronn.de>
2024-12-05 20:33:11 +0100

    norminette acceptable fdf

--- fdf.c
+++ fdf.c
@@ -12,7 +12,7 @@
 
 #include "fdf.h"
 
-void set_values_fdf(t_fdf *fdf)
+void	set_values_fdf(t_fdf *fdf)
 {
 	fdf->mlx = mlx_init(WIDTH, HEIGHT, "FdF", true);
 	fdf->canvas = mlx_new_image(fdf->mlx, WIDTH, HEIGHT);
@@ -21,45 +21,45 @@ void set_values_fdf(t_fdf *fdf)
 	fdf->shift_y = 300;
 }
 
-int	main(int argc, char **argv)
+void	add_hooks(t_fdf *fdf)
 {
-	t_fdf	*fdf;
-
-	if (argc != 2 || check_file_extension(argv[1]) == 0)
-	{
-		ft_putendl_fd("Usage: ./fdf <file.fdf>", 2);
-		return (EXIT_FAILURE);
-	}
-	fdf = allocate_memory_and_set_memory(argv[1]);
-	if (!fdf || !fdf->map_node)
-	{
-		ft_putendl_fd("Error: Failed to allocate memory or set matrix dimensions.", 2);
-		return (EXIT_FAILURE);
-	}
-	process_file(argv[1], fdf);
-	set_values_fdf(fdf);
-	if (!fdf->mlx)
-	{
-		ft_putendl_fd("Error: Failed to initialize mlx.", 2);
-		free_matrix(fdf->map_node, fdf->length);
-		free(fdf);
-		return (EXIT_FAILURE);
-	}
-	draw_matrix(fdf);
-	if (mlx_image_to_window(fdf->mlx, fdf->canvas, 0, 0) < 0)
-	{
-		ft_putendl_fd("Error: Failed to render image to window.", 2);
-		mlx_terminate(fdf->mlx);
-		free_matrix(fdf->map_node, fdf->length);
-		free(fdf);
-		return (EXIT_FAILURE);
-	}
 	mlx_loop_hook(fdf->mlx, &hook, fdf);
 	mlx_scroll_hook(fdf->mlx, &mouse_hook, fdf);
 	mlx_loop_hook(fdf->mlx, &key_hook, fdf);
 	mlx_loop(fdf->mlx);
 	mlx_terminate(fdf->mlx);
+}
+
+void	free_before_end(t_fdf *fdf)
+{
 	free_matrix(fdf->map_node, fdf->length);
 	free(fdf);
+}
+
+int	main(int argc, char **argv)
+{
+	t_fdf	*fdf;
+
+	if (argc != 2 || check_file_extension(argv[1]) == 0)
+		return (ft_putendl_fd("Usage: ./fdf <file.fdf>", 2), EXIT_FAILURE);
+	fdf = allocate_memory_and_set_memory(argv[1]);
+	if (!fdf || !fdf->map_node)
+		return (ft_putendl_fd("Err:Failed memory allocation", 2), EXIT_FAILURE);
+	process_file(argv[1], fdf);
+	set_values_fdf(fdf);
+	if (!fdf->mlx)
+	{
+		ft_putendl_fd("Err: Failed to initialize mlx.", 2);
+		return (free_before_end(fdf), EXIT_FAILURE);
+	}
+	draw_matrix(fdf);
+	if (mlx_image_to_window(fdf->mlx, fdf->canvas, 0, 0) < 0)
+	{
+		ft_putendl_fd("Err: Failed to render image to window.", 2);
+		mlx_terminate(fdf->mlx);
+		return (free_before_end(fdf), EXIT_FAILURE);
+	}
+	add_hooks(fdf);
+	free_before_end(fdf);
 	return (0);
 }
