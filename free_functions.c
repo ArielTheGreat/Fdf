313c8fb4ebffff9ba689b5bbef150fb72b02ec7e
frocha <frocha@student.42heilbronn.de>
2024-12-06 16:11:41 +0100

    apply norminette to free_funcitons file

--- free_functions.c
+++ free_functions.c
@@ -25,29 +25,30 @@ void	free_split_arrays(char **filename_extension)
 	free(filename_extension);
 }
 
-
-void free_matrix(t_map_node **map_node, int length, int width)
+void	free_matrix(t_map_node **map_node, int length, int width)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    if (!map_node)
-        return;
-
-    for (i = 0; i < length; i++)
-    {
-        for (j = 0; j < width; j++)
-        {
-            if (map_node[i][j].color)
-            {
-                free(map_node[i][j].color);
-                map_node[i][j].color = NULL;
-            }
-        }
-        free(map_node[i]);
-        map_node[i] = NULL;
-    }
-
-    free(map_node);
-    map_node = NULL;
+	if (!map_node)
+		return ;
+	i = 0;
+	while (i < length)
+	{
+		j = 0;
+		while (j < width)
+		{
+			if (map_node[i][j].color)
+			{
+				free(map_node[i][j].color);
+				map_node[i][j].color = NULL;
+			}
+			j++;
+		}
+		free(map_node[i]);
+		map_node[i] = NULL;
+		i++;
+	}
+	free(map_node);
+	map_node = NULL;
 }
